e9c5da5dc96d38dccc9c12af74caf0261213bcfa
pisacicmarko <marko.pisacic@fer.hr>
2020-08-03 20:28:18 +0200

    Use @PostConstruct in SHAApplication class & Use context to fetch Spring bean

--- src/main/java/com/ag04smarts/sha/SHAApplication.java
+++ src/main/java/com/ag04smarts/sha/SHAApplication.java
@@ -1,12 +1,36 @@
 package com.ag04smarts.sha;
 
+import com.ag04smarts.sha.bootstrap.BootStrapData;
+import com.ag04smarts.sha.domain.Patient;
+import com.ag04smarts.sha.repositories.PatientRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SHAApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SHAApplication.class, args);
-	}
+    private final BootStrapData bootStrapData;
+
+    public SHAApplication(BootStrapData bootStrapData) {
+        this.bootStrapData = bootStrapData;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        bootStrapData.initData();
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SHAApplication.class, args);
+
+        PatientRepository patientRepository = (PatientRepository) ctx.getBean("patientRepository");
+
+        System.out.println(patientRepository.findAll().toString());
+
+
+        System.out.println("Done");
+    }
 }
